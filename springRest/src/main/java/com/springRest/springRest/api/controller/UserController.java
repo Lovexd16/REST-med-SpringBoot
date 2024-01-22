2c4768083dc54f6893ccf7cc1cfedc7cc3565ac8
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-01-22 16:51:59 +0100

    Sätt upp REST api med Spring Boot

--- springRest/src/main/java/com/springRest/springRest/api/controller/UserController.java
+++ springRest/src/main/java/com/springRest/springRest/api/controller/UserController.java
@@ -1,8 +1,25 @@
 package com.springRest.springRest.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springRest.springRest.api.model.User;
+import com.springRest.springRest.service.UserService;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
     
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+        return userService.getUser(id);
+    }
 }
--- springRest/src/main/java/com/springRest/springRest/service/UserService.java
+++ springRest/src/main/java/com/springRest/springRest/service/UserService.java
@@ -1,8 +1,30 @@
 package com.springRest.springRest.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.springRest.springRest.api.model.User;
 
 @Service
 public class UserService {
     
+    private List<User> userList;
+
+    public UserService() {
+        userList = new ArrayList<>();
+        userList.add(new User(1, "Kalle", 20));
+        userList.add(new User(2, "Anna", 30));
+        userList.add(new User(3, "Bengt", 29));
+        userList.add(new User(4, "Kajsa", 56));
+    }
+
+    public User getUser(Integer id) {
+        return userList.stream()
+        .filter(user -> id.equals(user.getId()))
+        .findFirst()
+        .orElse(null);
+    }
+
 }
